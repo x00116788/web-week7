a284ffa0501e7da722ac93a62f7e963fb74041d7
suliat <x00116788@gmail.com>
2016-12-19 22:27:10 +0000

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TestNewComponent } from './test-new/test-new.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestNewComponent
   ],
   imports: [
     BrowserModule,
